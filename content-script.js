a6ec4175ca651a958a25a5e7f578c34f12b335b7
Cocoa <i@uwucocoa.moe>
2023-02-17 12:59:55 +0000

    early returns

--- content-script.js
+++ content-script.js
@@ -4,51 +4,59 @@ const observeUrlChange = () => {
   let oldPlayer = undefined
   let oldTime = -1
 
-  const handleProgressEvent = (event) => {
-    if (event.target.currentTime - oldTime > 1 || event.target.currentTime < oldTime) {
-      let title = 'unknown'
-
-      try {
-        title = document.querySelector("#title>h1>yt-formatted-string").innerHTML
-      } catch (error) {}
-  
-      chrome.runtime.sendMessage({
-        type: 'youtube',
-        data: oldPlaying,
-        current_time: event.target.currentTime,
-        duration: event.target.duration,
-        title: title
-      }, (resp) => {})
-
-      oldTime = event.target.currentTime
+  const handleTimeUpdateEvent = (event) => {
+    if (event.target.currentTime - oldTime < 1 ||
+        event.target.currentTime > oldTime) {
+      return
     }
+
+    let title = 'unknown'
+
+    try {
+      title = document.querySelector("#title>h1>yt-formatted-string").innerHTML
+    } catch (error) {}
+
+    chrome.runtime.sendMessage({
+      type: 'youtube',
+      data: oldPlaying,
+      current_time: event.target.currentTime,
+      duration: event.target.duration,
+      title: title
+    }, (resp) => {})
+
+    oldTime = event.target.currentTime
   }
 
   const body = document.querySelector("body")
   const observer = new MutationObserver(mutations => {
     mutations.forEach(() => {
-      if (oldHref !== document.location.href) {
-        oldHref = document.location.href
-        const url = new URL(document.location.href)
-        if (url !== undefined) {
-          if (url.hostname === 'www.youtube.com' && url.pathname === '/watch') {
-            let playing = url.searchParams.get('v');
-            if (playing !== undefined && playing.length > 0) {
-              let video = document.querySelector('video')
-
-              if (video !== undefined) {
-                if (oldPlayer !== undefined) {
-                  oldPlayer.removeEventListener('timeupdate', handleProgressEvent)
-                }
-                oldPlaying = playing
-                video.addEventListener('timeupdate', handleProgressEvent)
-              }
-
-              oldPlayer = video
-            }
-          }
-        }
+      if (oldHref === document.location.href) {
+        return
       }
+
+      oldHref = document.location.href
+      const url = new URL(document.location.href)
+      if (url === undefined ||
+          url.hostname !== 'www.youtube.com' ||
+          url.pathname !== '/watch') {
+        return
+      }
+
+      let playing = url.searchParams.get('v');
+      if (playing === undefined || playing.length === 0) {
+        return
+      }
+
+      let video = document.querySelector('video')
+      if (video !== undefined) {
+        if (oldPlayer !== undefined) {
+          oldPlayer.removeEventListener('timeupdate', handleTimeUpdateEvent)
+        }
+        oldPlaying = playing
+        video.addEventListener('timeupdate', handleTimeUpdateEvent)
+      }
+
+      oldPlayer = video
     })
   })
   observer.observe(body, { childList: true, subtree: true })
